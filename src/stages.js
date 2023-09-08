5650bee4e78065f14186aab1aa89706c8a1ea694
nicklzx <48320374+inekit@users.noreply.github.com>
2023-09-08 13:18:07 +0300

    change ports

--- src/stages.js
+++ src/stages.js
@@ -63,7 +63,7 @@ chatStage.on("message", async (ctx) => {
   const user_id = ctx.from.id;
   const chat_id = ctx.chat.id;
 
-  console.log(await ctx.botInfo());
+  console.log(await ctx.telegram.botInfo());
 
   const botInfo = await ctx.getChatMember(1971558222);
   console.log(botInfo);
